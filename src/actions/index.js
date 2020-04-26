43b6da1b6f61f63d731f3319b667342b76aeb2fd
AmberRamirez711 <amberramirez@icloud.com>
2020-04-26 11:58:59 -0700

    setup redux dectools and fixed reducer for set.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SET_RECENT_POSTS } from './types';
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from './types';
 
 import Axios from "axios"
 
@@ -6,11 +6,23 @@ export function fetchRecentPosts() {
     return function(dispatch) {
         Axios.get('https://api.dailysmarty.com/posts')
             .then(response => {
-                console.log(response.data.posts);
                 dispatch({
                     type: SET_RECENT_POSTS,
                     payload: response.data.posts
                 })
+            })
+    }
+}
+
+export function fetchPostsWithQuery(query, callback) {
+    return function(dispatch) {
+        Axios.get(`https://api.dailysmarty.com/search?q=${query}`)
+            .then(response => {
+                dispatch({
+                    type: SET_RESULTS_POSTS,
+                    payload: response.data.posts
+                })
+                if(callback) { callback() }
             })
     }
 }
